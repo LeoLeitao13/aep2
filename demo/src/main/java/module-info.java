aff3e4b14679a25295c153aae3a2a50edac97b41
Leonardo Leitao <102124645+LeoLeitao13@users.noreply.github.com>
2025-06-14 16:19:28 -0300

    Delete demo directory

--- demo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.demo {
-    requires javafx.fxml;
-    requires javafx.web;
-    requires jdk.jsobject;
-
-
-    opens org.example.demo to javafx.fxml;
-    exports org.example.demo;
-}
